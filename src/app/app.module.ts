4da565155b20fb4870767f4c8ffccb860d6b330f
iamhaji <hajisky07@gmail.com>
2018-09-09 23:28:01 +0300

    added change functonality

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,6 +4,11 @@ import { IonicApp, IonicErrorHandler, IonicModule } from 'ionic-angular';
 import { SplashScreen } from '@ionic-native/splash-screen';
 import { StatusBar } from '@ionic-native/status-bar';
 
+
+import { NetworkInterface } from '@ionic-native/network-interface';
+
+
+
 import { MyApp } from './app.component';
 import { HomePage } from '../pages/home/home';
 
@@ -24,7 +29,8 @@ import { HomePage } from '../pages/home/home';
   providers: [
     StatusBar,
     SplashScreen,
-    {provide: ErrorHandler, useClass: IonicErrorHandler}
+    {provide: ErrorHandler, useClass: IonicErrorHandler},
+    NetworkInterface
   ]
 })
 export class AppModule {}
--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,25 +1,49 @@
-import { Component } from '@angular/core';
-import { NavController } from 'ionic-angular';
+import { Component } from "@angular/core";
+import { NavController } from "ionic-angular";
+import { NetworkInterface } from "@ionic-native/network-interface";
 
 @Component({
-  selector: 'page-home',
-  templateUrl: 'home.html'
+  selector: "page-home",
+  templateUrl: "home.html"
 })
 export class HomePage {
-  status: Status
-  constructor(public navCtrl: NavController) {
+  status: Status;
+  motion: number;
+  ipaddress: string;
+  constructor(public navCtrl: NavController, public netInt: NetworkInterface) {
     this.status = {
       name: "close-circle",
       color: "danger",
       message: "not connected"
-    }
-  }
-  
+    };
 
+    this.motion = 0;
+
+    const url = "www.google.com";
+    this.netInt
+      .getHttpProxyInformation(url)
+      .then(proxy =>
+        console.info(
+          `Type: ${proxy.type}, Host: ${proxy.host}, Port: ${proxy.port}`
+        )
+      )
+      .catch(error => console.error(`Unable to get proxy info: ${error}`));
+  }
+
+  change(value: number) {
+    this.motion = value;
+  }
+  send() {
+    console.log(this.motion);
+  }
+  save(){
+    // TODO- SAVE IP ADDRESS
+  }
 }
 
-interface Status{
-  name: string,
-  color: string,
-  message: string
+
+interface Status {
+  name: string;
+  color: string;
+  message: string;
 }
